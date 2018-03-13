9952e604461ce654f694886d06b592a214df4a2b
Maiken Guldberg <maiken.g@fibermail.dk>
2018-03-13 09:25:35 +0100

    Added a integration test class

---
+++ Microwave.Test.Intergration/IT1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace Microwave.Test.Intergration
+{
+    [TestFixture]
+    public class IT1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+
+
+    }
+}
